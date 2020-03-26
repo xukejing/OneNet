db20449e27ade05b3c2e6934799670f776da2c15
xukejing <648460636@qq.com>
2020-03-26 11:25:46 +0800

    Create gps.js

---
+++ View2.0Demo/gps.js
@@ -0,0 +1,12 @@
+var result = [];
+var dataPoint = null;
+var i = 0;
+  dataPoint = {
+    dev1: {
+      lon: 126.64,
+      lat: 45.75,//定义经纬度
+    },
+  };
+  result.push(dataPoint);
+
+return result;
